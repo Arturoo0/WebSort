dcb2e5f9190768be3e288b9003ae02b83a11b96e
Arturoo0 <arturoportelles@gmail.com>
2020-02-23 23:09:40 -0500

    added basic draw rect draw

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 let button = document.querySelector("#button");
+let canvas = document.querySelector("canvas")
+let c = canvas.getContext('2d');
 
-button.onclick = () => {
-  console.log("1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight * 0.7 ;
+
+let xTracker = 0;
+let increment = canvas.width/10;
+
+lst = [100, 30, 57, 45, 33, 67, 88, 98, 32, 13];
+
+for (let i = 0; i < 10; i++){
+  c.fillRect(xTracker, 0, increment, lst[i]);
+  xTracker += increment;
 }
+
+// button.onclick = () => {
+//   console.log("1");
+// }
